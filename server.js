4537bb60ca69920effdea824d215d208263fb4ac
Michiocre <michi2806@gmail.com>
2021-01-09 14:13:08 +0100

    Update server

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 require('dotenv').config();
 
 var filename = '';
-
 fs.readdir(__dirname, (err, files) => {
     files.forEach(file => {
         if (file.split('.')[1] == 'csv') {
@@ -19,17 +18,17 @@ app.use('/data.csv', function(req, res) {
     if (filename != '') {
         res.sendFile(path.join(__dirname+'/'+filename));
     } else {
-        res.send('No .csv file was found on the server');
-    }    
-})
+        res.send("Es wurde keine .csv Datei auf dem Server gefunden.");
+    }
+});
 
 app.use('/style.css', function(req, res) {
     res.sendFile(path.join(__dirname+'/style.css'));
-})
+});
 
 app.use('/favicon.ico', function(req, res) {
     res.sendFile(path.join(__dirname+'/favicon.ico'));
-})
+});
 
 app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/index.html')); 
